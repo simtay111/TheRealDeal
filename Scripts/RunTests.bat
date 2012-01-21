44a69429fdcd7ab44e0900308cfd4e4bcb9c5fb5
Simon Taylor <simtay111@gmail.com>
2012-01-21 00:11:03 -0800

    Upgraded test scripts to use a proj file

--- Scripts/RunTests.bat
+++
@@ -1,4 +0,0 @@
-cd C:\Program Files (x86)\TestDriven.NET 3\NUnit\2.5
-nunit-console C:\projects\recreateme\tests\recreatemetests.csproj /labels
-nunit-console C:\projects\RecreateMeSql\RecreateMeSqlTests\RecreateMeSqlTests.csproj /labels
-cd c:\Projects\Scripts
