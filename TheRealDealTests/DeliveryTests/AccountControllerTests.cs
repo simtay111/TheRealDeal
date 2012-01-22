17ecff98075d6734d6e44798b33e5fc4712e84c1
Simon Taylor <simtay111@gmail.com>
2012-01-21 16:28:02 -0800

    Attempting to test MVC controllers... failing...

---
+++ TheRealDealTests/DeliveryTests/AccountControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using TheRealDeal.Controllers;
+using TheRealDeal.Models.Account;
+
+namespace TheRealDealTests.DeliveryTests
+{
+    [TestFixture]
+    public class AccountControllerTests
+    {
+        [Test]
+        [Ignore]
+        public void CanLogin()
+        {
+            var controller = new AccountController();
+            var model = new LogOnModel()
+                            {
+                                Password = "123",
+                                UserName = "UserName"
+                            };
+
+            var actionResult = controller.LogIn(model);
+
+            Assert.That(actionResult, Is.Not.Null);
+        }
+    }
+}
