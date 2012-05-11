e1395038fcb9f7ecec7f5f0c2f43a8da79f0d903
Simon Taylor <simtay111@gmail.com>
2012-05-10 21:13:28 -0700

    moo

--- RecreateMeSql/RecreateMeSql/ConnectionFactory.cs
+++ RecreateMeSql/RecreateMeSql/ConnectionFactory.cs
@@ -14,7 +14,7 @@ namespace RecreateMeSql
 
         static ConnectionFactory()
         {
-            _factory = new OrmLiteConnectionFactory(@"Server=(local);Database=PortlandPickUp;Trusted_Connection=True;", SqlServerOrmLiteDialectProvider.Instance);
+            _factory = new OrmLiteConnectionFactory(@"Server=e2babdc8-29c6-4349-a215-a04b000468e0.sqlserver.sequelizer.com;Database=dbe2babdc829c64349a215a04b000468e0;User ID=lyyfhvctjjrscbps;Password=XhHC4xWZbpWheqSSQCJbJfTB6YPctfUoHgZxncwcihmcc8QeK5bKAJhRvfFeRUF4;", SqlServerOrmLiteDialectProvider.Instance);
         }
 
         public static OrmLiteConnectionFactory Create()
