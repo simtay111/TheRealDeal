cb1abe79103af1a56a6794188a8b75342113c1a6
Simon Taylor <simtay111@gmail.com>
2012-05-15 12:48:31 -0700

    trying to fix things

--- TheRealDealTests/DataTests/TestSetUpAndTeardown.cs
+++ TheRealDealTests/DataTests/TestSetUpAndTeardown.cs
@@ -15,8 +15,6 @@ namespace TheRealDealTests.DataTests
             //Server=(local);Database=PortlandPickUp;Trusted_Connection=True;
             ConnectionFactory.SetConnectionString(@"Server=e68a7e51-aaa0-4f84-9b24-a052015234b6.sqlserver.sequelizer.com;Database=dbe68a7e51aaa04f849b24a052015234b6;User ID=lthacnqfszvhnhoa;Password=JxCbALoiiQoHc8pi5uurZH3CPUNvhSNNttonPvwXEi2yqdjTPQvk7rhbC8uN7Zax;");
             SqlServerDataHelper.RebuildSchema();
-
-            
         }
 
         [TearDown]
