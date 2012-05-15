f149767aa7fd668ae7a159fe48e91bfa4fd8c922
Simon Taylor <simtay111@gmail.com>
2012-05-14 18:27:32 -0700

    Setting up connection factory to use System.Configuration

--- RecreateMeSql/RecreateMeSql/ConnectionFactory.cs
+++ RecreateMeSql/RecreateMeSql/ConnectionFactory.cs
@@ -1,5 +1,6 @@
 ﻿using ServiceStack.OrmLite;
 using ServiceStack.OrmLite.SqlServer;
+using System.Configuration;
 
 namespace RecreateMeSql
 {
@@ -10,8 +11,8 @@ namespace RecreateMeSql
         static ConnectionFactory()
         {
             //_factory = new OrmLiteConnectionFactory(@"Server=(local);Database=PortlandPickUp;Trusted_Connection=True;", SqlServerOrmLiteDialectProvider.Instance);
-            _factory = new OrmLiteConnectionFactory(@"Server=e2babdc8-29c6-4349-a215-a04b000468e0.sqlserver.sequelizer.com;Database=dbe2babdc829c64349a215a04b000468e0;User ID=lyyfhvctjjrscbps;Password=XhHC4xWZbpWheqSSQCJbJfTB6YPctfUoHgZxncwcihmcc8QeK5bKAJhRvfFeRUF4;", SqlServerOrmLiteDialectProvider.Instance);
-
+            _factory = new OrmLiteConnectionFactory(ConfigurationManager.ConnectionStrings["PortlandPickUp"].ConnectionString, SqlServerOrmLiteDialectProvider.Instance);
+            
         }
 
         public static OrmLiteConnectionFactory Create()
